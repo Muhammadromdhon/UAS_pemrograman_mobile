c520910511764b8269ac280bfe44bdb791f7e245
Muhammad Romdhon <72908619+Muhammadromdhon@users.noreply.github.com>
2023-07-12 21:18:25 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -37,7 +37,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('312110388-Muhammad Alwi Nur Fathihah'),
+          title: Text('Muhammad Romdhon - 312010434'),
         ),
         body: ListView.builder(
           itemCount: stations.length,
